8088b90ae9a7c4f27376d7ba9b55d324e45552c5
Andrew Dunkle <andrewdunkle@Andrews-MacBook-Pro.local>
2023-04-10 14:24:14 -0700

    Fixing Dockfile reference

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,8 +7,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    name = os.environ.get("NAME", "World")
+    return f"Hello {name}!"
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
